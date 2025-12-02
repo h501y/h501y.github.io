46f9b68d7daa9afa5092c7fdbc68eb467c442b30
h501y <h501y@users.noreply.github.com>
2025-12-02 13:36:31 +0000

    deploy: 24f0d801d6c1ee730f3782a4da702ab6487033df

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Increment this version number with each deployment to force cache refresh
-const CACHE_VERSION = 9;
+const CACHE_VERSION = 10;
 const CACHE_NAME = `mtg-collection-v${CACHE_VERSION}`;
 const DATA_CACHE_NAME = `mtg-data-v${CACHE_VERSION}`;
 
